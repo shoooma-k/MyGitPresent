60892b8895c64ad1e54e036b4c567e3d051d3c17
Shoma <shoma.kaji@gmail.com>
2025-08-29 14:11:36 +0900

    ナンバータッチ追加

--- main.js
+++ main.js
@@ -29,6 +29,7 @@
   const contentList = document.getElementById('contentList');
   const comments = document.getElementById('comments');
   const bye = document.getElementById('bye');
+  const numbertuch = document.getElementById('numbertuch');
   const typing = document.getElementById('typing');
   const slot = document.getElementById('slot');
 
@@ -37,6 +38,7 @@
   contentList.hidden = true;
   comments.hidden = true;
   bye.hidden = true;
+  numbertuch.hidden = true;
   typing.hidden = true;
   slot.hidden = true;
 
@@ -92,6 +94,151 @@
   const listNumbertuch = document.getElementById('list-numbertuch');
   const listSlot = document.getElementById('list-slot');
   const listLyricsTyping = document.getElementById('list-lyrics-typing');
+
+  listNumbertuch.addEventListener('click', () => {
+
+    const levels = Number(prompt('レベルを設定してください。 ※レベル1〜6'));
+    
+    if (levels > 6) {
+      return;
+    }
+
+    class Panel {
+      constructor(game) {
+        this.game = game;
+        this.el = document.createElement('li');
+        this.el.classList.add('pressed');
+        this.el.addEventListener('click', () => {
+          this.check();
+        })
+      }
+
+      getEl() {
+        return this.el;
+      }
+
+      activate(num) {
+        this.el.classList.remove('pressed');
+        this.el.textContent = num;
+      }
+
+      check() {
+        if (this.game.getCurrentNum() === parseInt(this.el.textContent, 10)) {
+          this.el.classList.add('pressed');
+          this.game.addCurrentNum();
+
+          if (this.game.getCurrentNum() === this.game.getLevel() ** 2) {
+            clearTimeout(this.game.getTimeoutId());
+          }
+        }
+      }
+    }
+
+    class Board {
+      constructor(game) {
+        this.game = game;
+        this.panels = [];
+        for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+          this.panels.push(new Panel(this.game));
+        }
+        this.setup();
+      }
+
+      setup() {
+        const board = document.getElementById('board');
+        this.panels.forEach(panel => {
+          board.appendChild(panel.getEl());
+        });
+      }
+
+      activate() {
+        const nums = [];
+        for (let i = 0; i < this.game.getLevel() ** 2; i++) {
+          nums.push(i);
+        }
+
+
+          this.panels.forEach(panel => {
+            const num = nums.splice(Math.floor(Math.random() * nums.length), 1)[0];
+            panel.activate(num);
+          });
+        }
+      }
+
+
+    class Game {
+      constructor(level) {
+        this.level = level;
+        this.board = new Board(this);
+
+        this.currentNum = undefined;
+        this.startTime = undefined;
+        this.timeoutId = undefined;
+
+        const btn = document.getElementById('btn');
+        btn.addEventListener('click', () => {
+          this.start();
+        });
+        this.setup();
+      }
+
+      setup() {
+        const container = document.getElementById('container');
+        const PANEL_WIDTH = 50;
+        const BOARD_PADDING = 10;
+        container.style.width = PANEL_WIDTH * this.level + BOARD_PADDING * 2 + 'px';
+      }
+
+      start() {
+        if (typeof this.timeoutId !== 'undefined') {
+          clearTimeout(this.timeoutId);
+        }
+
+        this.currentNum = 0;
+        this.board.activate();
+
+        this.startTime = Date.now();
+        this.runTimer();
+      }
+
+      runTimer() {
+        const timer = document.getElementById('timer');
+        timer.textContent = ((Date.now() - this.startTime) / 1000).toFixed(2);
+
+        this.timeoutId = setTimeout(() => {
+          this.runTimer();
+        }, 10);
+      }
+
+      addCurrentNum() {
+        this.currentNum++;
+      }
+
+      getCurrentNum() {
+        return this.currentNum;
+      }
+
+      getTimeoutId() {
+        return this.timeoutId;
+      }
+
+      getLevel() {
+        return this.level;
+      }
+    }
+
+    new Game(levels + 1);
+
+
+    loginPage.hidden = true;
+    welcome.hidden = true;
+    contentList.hidden = true;
+    comments.hidden = true;
+    bye.hidden = true;
+    numbertuch.hidden = false;
+    typing.hidden = true;
+    slot.hidden = true;
+  })
 
 
   listLyricsTyping.addEventListener('click', () => {
